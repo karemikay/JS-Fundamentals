fa921cb22976cb9388f2e2a126204c396fa5fa5c
karemikay <karemikay2@gmail.com>
2025-11-14 12:10:44 +0300

    Add argument-check script and update other JS files

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+// Get the arguments passed to the script (excluding 'node' and the filename)
+const args = process.argv.slice(2);
+
+// Check the number of arguments and print the appropriate message
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}

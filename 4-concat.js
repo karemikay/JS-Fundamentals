1b0b2da1047bcc4f4c1ceaa2d34a3fa7226fadb4
karemikay <karemikay2@gmail.com>
2025-11-14 13:16:20 +0300

    Fix 4-concat.js to match checker expected output

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,5 @@
-// Get the arguments passed to the script (skip node and filename)
+// Get the arguments passed to the script (skip 'node' and filename)
 const args = process.argv.slice(2);
 
-if (args[0] === undefined) {
-  console.log("No argument");
-} else if (args[1] === undefined) {
-  console.log(args[0] + " is");
-} else {
-  console.log(args[0] + " is " + args[1]);
-}
+// Print the first two arguments exactly
+console.log(args[0] + " is " + args[1]);
